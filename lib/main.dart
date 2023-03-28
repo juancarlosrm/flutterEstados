d7b0b0b9686e5b9e6dd93b326605b439259357d7
Juanca <carlos.rm.riot@gmail.com>
2023-03-28 11:17:26 -0600

    primer commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_singleton/pages/pagina1_page.dart';
+import 'package:flutter_singleton/pages/pagina2_page.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Material App',
+      initialRoute: 'pagina1',
+      routes: {
+        'pagina1': (_) => const Pagina1Page(),
+        'pagina2': (_)=> const Pagina2Page()
+      },
+    );
+  }
+}
---
+++ lib/models/usuario.dart
@@ -0,0 +1,10 @@
+
+
+class Usuario {
+  String nombre;
+  int edad;
+  List<String> profesiones;
+
+  Usuario({required this.nombre, required this.edad, required this.profesiones});
+
+}
